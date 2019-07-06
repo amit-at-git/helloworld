c7157bde7fc6622d0c311e6a898a5a9246698a82
Amit Chauhan <chauhan_amit@yahoo.com>
2019-07-06 14:28:57 -0400

    Added Car Class and referred in Main method.

--- src/main/java/org/amitc/jenkins/helloworld/App.java
+++ src/main/java/org/amitc/jenkins/helloworld/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Car car = new Car("blue", "bmw");
+        System.out.println("Car color is " + car.getColor());
+        System.out.println("Car model is " + car.getModel());
+        
     }
 }
